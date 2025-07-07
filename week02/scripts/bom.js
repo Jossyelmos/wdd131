ff348b9180ed1f0bfe41119ed4c2654a9fd4d326
jossyelmos <jossyelmos@yahoo.com>
2025-07-07 20:02:59 +0100

    event dom manipulation

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,22 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('#list'); // you need to fill in the blank to reference the HTML element that is a unordered list element.
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        })
+        input.focus();
+        input.value = '';
+    }
+})
